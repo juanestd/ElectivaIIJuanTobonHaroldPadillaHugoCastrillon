8b4467cade8db6def7eaa008f5b17e6650cd1990
Harold Padilla <hypadilla@hotmail.com>
2024-08-10 09:17:05 -0500

    added authControllers and authMocks

---
+++ controllers/authControllers.js
@@ -0,0 +1,29 @@
+const users = require('../mocks/authMocks');
+
+const registerUser = (req, res) => {
+    const { username, password } = req.body;
+    const user = { id: new Date().getMilliseconds(), username, password };
+    users.push(user);
+    res.status(201).json({ message: "User registered successfully", user });
+};
+
+const loginUser = (req, res) => {
+    const { username, password } = req.body;
+
+    if (!username || !password) {
+        return res.status(400).json({status: "error", message: "Username and password are required" });
+    }
+
+    const user = users.find(user => user.username === username && user.password === password);
+
+    if (user) {
+        res.status(200).json({status: "ok" ,message: "Login successful", user });
+    } else {
+        res.status(401).json({status: "error", message: "Invalid credentials" });
+    }
+};
+
+module.exports = {
+    registerUser,
+    loginUser,
+};
