c524de4da19900f9047fef520f9586fe98fd89cb
Harold Padilla <hypadilla@hotmail.com>
2024-08-10 09:51:47 -0500

    added postsControllers

---
+++ mocks/postMocks.js
@@ -0,0 +1,25 @@
+const posts = [
+    {
+        id: 1,
+        username: "user1",
+        fullname: "First User",
+        date: '2024-08-10T10:30:00Z',
+        message: "This is the body of the first post."
+    },
+    {
+        id: 2,
+        username: "user2",
+        fullname: "Second User",
+        date: '2024-08-10T11:00:00Z',
+        message: "This is the body of the second post."
+    },
+    {
+        id: 3,
+        username: "user1",
+        fullname: "First User",
+        date: '2024-08-10T12:00:00Z',
+        message: "This is another post by user1."
+    }
+];
+
+module.exports = posts;
